5afd299eeabdb300a04729b340ba1865d8125ac8
Samson Endale <4sam21@gmail.com>
2021-08-03 16:49:11 +0300

    [WIP] Added more cases

--- tests/Feature/CsvImportCommandTest.php
+++ tests/Feature/CsvImportCommandTest.php
@@ -1,7 +1,16 @@
 <?php
 
-it('has csv:import command', function () {
-    $this->artisan('csv:import')
-        ->assertExitCode(0);
+it('errors when incorrect path is given', function () {
+    $this->artisan('csv:import /path/to/a/non-existent/file')
+        ->expectsOutput('File does not exist!')
+        ->assertExitCode(-1);
 });
 
+it('parse csv files', function () {
+    /** @var \Tests\TestCase $this */
+    $this->artisan('csv:import stubs/simple.csv')
+        ->expectsTable(['Name', 'Handle'], [
+            ['Samson Endale', 'SamAsEnd'],
+        ])
+        ->assertExitCode(0);
+});
