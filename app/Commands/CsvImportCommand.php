469a91f36d7dd801b7a45ed4c1e58cec1f83ec2c
Samson Endale <4sam21@gmail.com>
2021-08-03 17:42:14 +0300

    Pass the test

--- app/Commands/CsvImportCommand.php
+++ app/Commands/CsvImportCommand.php
@@ -2,8 +2,10 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use League\Csv\Exception;
+use League\Csv\Reader;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class CsvImportCommand extends Command
 {
@@ -12,7 +14,7 @@ class CsvImportCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'csv:import';
+    protected $signature = 'csv:import {path}';
 
     /**
      * The description of the command.
@@ -28,6 +30,22 @@ class CsvImportCommand extends Command
      */
     public function handle()
     {
-        //
+        $path = $this->argument('path');
+
+        try {
+            $csv = Reader::createFromPath($path);
+            $csv->setHeaderOffset(0);
+
+            $header = $csv->getHeader();
+            $records = collect($csv->getRecords())
+                ->map(fn($arr) => array_map('trim', $arr))
+                ->toArray();
+
+            $this->table($header, $records);
+        } catch (Exception $ex) {
+            $this->error('File does not exist!', OutputInterface::VERBOSITY_NORMAL);
+
+            return -1;
+        }
     }
 }
